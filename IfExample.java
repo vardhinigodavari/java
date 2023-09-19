8f359ea95eb1568c7f0840b3f8463388aff33dc5
vardhinigodavari <144759164+vardhinigodavari@users.noreply.github.com>
2023-09-18 21:37:07 -0700

    Add files via upload

---
+++ IfExample.java
@@ -0,0 +1,10 @@
+public class IfExample{
+       public static void main(String arg[]){
+             int x=10;
+             int y=12;
+             if(x+y>20)
+             {
+             System.out.println("x=y is greaterthan 20");
+             }
+       }
+}
