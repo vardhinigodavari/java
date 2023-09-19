3f043a7f1043d724d0677bf361f8a4f3e89171e9
vardhinigodavari <144759164+vardhinigodavari@users.noreply.github.com>
2023-09-18 21:38:49 -0700

    Add files via upload

---
+++ CmdLineArgs.java
@@ -0,0 +1,10 @@
+import java.io.*;
+piblic class CmdLineArgs extends Object {
+//Object is the super classes of java 
+	publis static void main(String args[]) {
+		int x = Integer.parseInt(args[0]);
+		int y = Integer.parseInt(args[1]);
+		int z = x+y;
+  system.out.println("Sum of" +x "and" +y "is" +z)
+ 	}
+}
