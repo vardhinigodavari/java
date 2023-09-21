65b37dffe8ac6403647b370a6dc92d340281dd2e
vardhinigodavari <144759164+vardhinigodavari@users.noreply.github.com>
2023-09-21 16:06:16 +0530

    Create MethodOverLodingDemo.java

---
+++ MethodOverLodingDemo.java
@@ -0,0 +1,20 @@
+import java.io.*;
+class Adder{
+    int add(int a,int b){
+        return a+b;
+    }
+    int add(int a,int b,int c){
+    return a+b+c;
+  }
+}
+public class MethodOverLoadingDemo{
+    public static void main(String args[]){
+        Adder ad = new Adder();
+        int x = ad.add(5,0);
+        int y = ad.add(10,20,30);
+        int z = ad.add(x,y);
+        System.out.println("x value is"+x);
+        System.out.println("y value is"+y);
+        System.out.println("z value is"+z);
+    }
+}
