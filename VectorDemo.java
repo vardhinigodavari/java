d92c893e428b94b6c1a27bf018fab5d7a8192f41
vardhinigodavari <144759164+vardhinigodavari@users.noreply.github.com>
2023-10-18 09:27:10 +0530

    Create VectorDemo.java

---
+++ VectorDemo.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class VectorDemo{
+    public static void main(String args[]){
+        Vector <String> vec = new Vector<String>();
+        vec.add("UMME");
+        vec.add("Samhitha");
+        vec.add("HAFSA");
+        vec.add("Vardhini");
+        vec.add("Narayana");
+        vec.add("Lavanaya");
+        System.out.println("The elements in the vector are "+vec);
+    }
+}
